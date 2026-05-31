26069aade906bcc305a3203ec7d0de8842529f4c
Laukik shewale <luckyshewale2004@gmail.com>
2026-05-31 23:40:17 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,20 @@
 function sendOrder() {
   let params = {
-  name: document.getElementById("name").value,
-  mobile: document.getElementById("mobile").value,
-  email: document.getElementById("email").value,
-  type: document.getElementById("type").value,
-  message: document.getElementById("message").value
-};
+    name: document.getElementById("name").value,
+    mobile: document.getElementById("mobile").value,
+    email: document.getElementById("email").value,
+    type: document.getElementById("type").value,
+    message: document.getElementById("message").value
+  };
 
-
-  emailjs.send("service_stfvqva","template_bpes4sj",params)
-  .then(()=>{
-    alert("✅ Order sent successfully! We will contact you soon.");
-    document.querySelector("form").reset();
-  });
-}
-
-function openPortfolioPage(){
-  window.location.href = "portfolio-demo.html";
-}
-function openBusinessPage(){
-  window.location.href = "business-demo.html";
-}
-function openEcommercePage(){
-  window.location.href = "ecommerce-demo.html";
+  emailjs.send("service_529eh0h", "template_bpes4sj", params)
+    .then(function(response) {
+      alert("✅ Order sent successfully!");
+      document.querySelector("form").reset();
+      console.log(response);
+    })
+    .catch(function(error) {
+      console.log("ERROR:", error);
+      alert("❌ " + JSON.stringify(error));
+    });
 }
